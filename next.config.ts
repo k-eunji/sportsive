948162c73b577f383d7c8ee7c4189ee2a3678ac2
Marina Kim <eunji9157@gmail.com>
2025-12-09 18:47:12 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // ⭐ 반드시 false로 해야 API 1번 호출됨!
   reactStrictMode: false,
 
-  // 🚀 빌드 속도 향상: 타입 에러 무시
   typescript: {
     ignoreBuildErrors: true,
   },
 
-  // 🚀 빌드 속도 향상: ESLint 에러 무시
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  turbopack: {},
 
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "crests.football-data.org",
-      },
+      { protocol: "https", hostname: "crests.football-data.org" },
       {
         protocol: "https",
         hostname: "firebasestorage.googleapis.com",
         pathname: "/v0/b/**",
       },
-      {
-        protocol: "https",
-        hostname: "firebasestorage.app",
-      },
+      { protocol: "https", hostname: "firebasestorage.app" },
       {
         protocol: "https",
         hostname: "storage.googleapis.com",
@@ -50,14 +39,6 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
-  },
-
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      path: false,
-    };
-    return config;
   },
 };
 
